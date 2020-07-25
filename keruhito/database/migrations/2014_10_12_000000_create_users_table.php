44b5f29e23f4cea56b5f7902867923d03665ad52
奥野龍 <okunoryo@MBAR.local>
2020-07-25 12:56:07 +0900

    users postsテーブル作成

--- keruhito/database/migrations/2014_10_12_000000_create_users_table.php
+++ keruhito/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable()->degfault(NULL);
+            $table->string('email')->unique()->nullable()->degfault(NULL);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable()->degfault(NULL);
             $table->rememberToken();
+            $table->string('twitter_id')->nullable()->degfault(NULL);
+            $table->string('access_token')->nullable()->degfault(NULL);
+            $table->string('access_token_secret')->nullable()->degfault(NULL);
+            $table->string('avatar')->nullable()->degfault(NULL);
             $table->timestamps();
         });
     }
---
+++ keruhito/database/migrations/2020_07_25_032632_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title')->nullable()->default(NULL);
+            $table->string('content')->nullable()->defalut(NULL);
+            $table->string('image')->nullable()->defalut(NULL);
+            $table->string('twitter_id')->nullable()->default(NULL);
+            $table->string('twitter_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
